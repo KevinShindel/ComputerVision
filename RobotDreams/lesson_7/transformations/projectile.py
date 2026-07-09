c68812fccc816657b6d296ae09b5610cd9ab7a50
Kevin Shindel <kevin.shindel@yahoo.com>
2026-07-09 22:06:04 +0200

    feat: implement face detection functionality and update requirements

--- RobotDreams/lesson_7/transformations/projectile.py
+++ RobotDreams/lesson_7/transformations/projectile.py
@@ -1,1 +1,6 @@
-# TODO: develop this
+# TODO: develop image tracking functionality
+
+# TODO: 1. Use Motion tracking for track object moving
+# TODO: 2. Calculate object center by bounding box
+# TODO: 3. Save center point for each frame
+# TODO: 4. Connect each points and draw line
